dc0871711d199c793da3a9ca9508fc50d0a0560c
mosayeh99 <mohamedelsayeh66@gmail.com>
2023-02-24 04:38:20 +0200

    autoplay & mute reels

--- Front/src/app/components/reels/reels.component.ts
+++ Front/src/app/components/reels/reels.component.ts
@@ -1,15 +1,18 @@
 import {Component, HostListener} from '@angular/core';
-
 @Component({
   selector: 'app-reels',
   templateUrl: './reels.component.html',
   styleUrls: ['./reels.component.css']
 })
 export class ReelsComponent {
+  isMuted:boolean = true;
   isLiked: boolean = false;
   isSaved: boolean = false;
   isFollowing: boolean = false;
   followingStatus = 'Follow';
+  ToggleVideoMute() {
+    this.isMuted = !this.isMuted;
+  }
   getFollowingStatus() {
     if(this.isFollowing) {
       this.followingStatus = 'Following';
