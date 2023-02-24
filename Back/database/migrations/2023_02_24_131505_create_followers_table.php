ebb8b85dbf24703e80eb09b86a44f195dc557d3d
mosayeh99 <mohamedelsayeh66@gmail.com>
2023-02-24 16:50:12 +0200

    create database tables

---
+++ Back/database/migrations/2023_02_24_131505_create_followers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('followers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id_self');
+            $table->unsignedBigInteger('user_id_other');
+            $table->foreign('user_id_self')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('user_id_other')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('followers');
+    }
+};
