bf4e5168aebaf49a0d24c8269a2489f0d7ea8dfc
AhmedHassan199 <ahmedhassan4855@gmail.com>
2023-03-03 18:43:15 +0200

    back massege commit

--- Back/routes/api.php
+++ Back/routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('fetch', [App\Http\Controllers\ChatsController::class, 'fetchMessages']);
+
+Route::post('send', [App\Http\Controllers\ChatsController::class, 'sendMessage']);
+Route::post('recive', [App\Http\Controllers\ChatsController::class, 'reciveMessage']);
+Route::get('users', [App\Http\Controllers\ChatsController::class, 'getUsers']);
